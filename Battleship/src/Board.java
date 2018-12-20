eb1cf12a9db1024cb047820a39f8fa6490a23ad1
mason.coon <mason.coon@MU-C1-15.mullen.pvt.k12.co.us>
2018-12-20 11:42:16 -0700

    Filling board with blank spaces

--- Battleship/src/Board.java
+++ Battleship/src/Board.java
@@ -1,11 +1,39 @@
 
 public class Board
 	{
+		static String[][] board = new String[5][5];
+		public static void fillBoard()
+		{
+			for ( int i = 0; i <= 5; i++)
+				{
+					System.out.println("Shut up cole");
+				}
+				
+		}
+
 		public static void display()
 			{
-System.out.println("  1    2    3    4    5");
-System.out.println(" ____ ____ ____ ____ ____");
-System.out.println("|    |    |    |    |    |");
-System.out.println("|    |    |    |    |    |");
+System.out.println("      1       2       3       4       5");
+System.out.println("   ------- ------- ------- ------- -------");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("1 |       |       |       |       |       |");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("   ------- ------- ------- ------- -------");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("2 |       |       |       |       |       |");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("   ------- ------- ------- ------- -------");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("3 |       |       |       |       |       |");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("   ------- ------- ------- ------- -------");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("4 |       |       |       |       |       |");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("   ------- ------- ------- ------- -------");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("5 |       |       |       |       |       |");
+System.out.println("  |       |       |       |       |       |");
+System.out.println("   ------- ------- ------- ------- -------");
 			}
 	}
